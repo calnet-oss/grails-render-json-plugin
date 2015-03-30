7216d2994ff3dc1298b52322b906c18fe6675954
Brian Koehmstedt <bkoehmstedt@berkeley.edu>
2015-03-30 11:47:28 -0700

    UTF-8 fix and add a test for UTF-8

--- grails-app/domain/edu/berkeley/render/json/test/TestPerson.groovy
+++ grails-app/domain/edu/berkeley/render/json/test/TestPerson.groovy
@@ -1,10 +1,11 @@
 package edu.berkeley.render.json.test
 
-class TestPerson
-{
-  String uid
-  String emailAddress
-  Date dateOfBirth
-  Date timeCreated
-  Date timeUpdated
+class TestPerson {
+    String uid
+    String emailAddress
+    Date dateOfBirth
+    Date timeCreated
+    Date timeUpdated
+    String firstName
+    String lastName
 }
