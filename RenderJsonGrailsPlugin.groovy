adeba9e19a7d80d4ede6b523d1eb79384df08ecc
Brian Koehmstedt <bkoehmstedt@berkeley.edu>
2016-08-11 18:06:04 -0700

    add links

--- RenderJsonGrailsPlugin.groovy
+++ RenderJsonGrailsPlugin.groovy
@@ -55,27 +55,26 @@ Provides extensions to the standard JSON converter to add rendering features.
 '''
 
     // URL to the plugin's documentation
-    //def documentation = "http://grails.org/plugin/grails-render-json"
+    def documentation = "https://github.com/calnet-oss/grails-render-json-plugin/"
 
     // Extra (optional) plugin metadata
 
     // License: one of 'APACHE', 'GPL2', 'GPL3'
-//    def license = "APACHE"
+    def license = "BSD"
 
     // Details of company behind the plugin (if there is one)
-//    def organization = [ name: "My Company", url: "http://www.my-company.com/" ]
+    def organization = [ name: "University of California, Berkeley", url: "http://www.berkeley.edu/" ]
 
     // Any additional developers beyond the author specified above.
 //    def developers = [ [ name: "Joe Bloggs", email: "joe@bloggs.net" ]]
 
     // Location of the plugin's issue tracker.
-//    def issueManagement = [ system: "JIRA", url: "http://jira.grails.org/browse/GPMYPLUGIN" ]
+    def issueManagement = [ system: "GitHub", url: "https://github.com/calnet-oss/grails-render-json-plugin/issues" ]
 
     // Online location of the plugin's browseable source code.
-//    def scm = [ url: "http://svn.codehaus.org/grails-plugins/" ]
+    def scm = [ url: "https://github.com/calnet-oss/grails-render-json-plugin" ]
 
     def doWithWebDescriptor = { xml ->
-        // TODO Implement additions to web.xml (optional), this event occurs before
     }
 
     // we put the doWithSpring closure in ExtendedJSON so that we can 
@@ -83,7 +82,6 @@ Provides extensions to the standard JSON converter to add rendering features.
     def doWithSpring = ExtendedJSON.doWithSpringRegisterMarshallersClosure
 
     def doWithDynamicMethods = { ctx ->
-        // TODO Implement registering dynamic methods to classes (optional)
     }
 
     def doWithApplicationContext = { ctx ->
@@ -96,17 +94,11 @@ Provides extensions to the standard JSON converter to add rendering features.
     }
 
     def onChange = { event ->
-        // TODO Implement code that is executed when any artefact that this plugin is
-        // watching is modified and reloaded. The event contains: event.source,
-        // event.application, event.manager, event.ctx, and event.plugin.
     }
 
     def onConfigChange = { event ->
-        // TODO Implement code that is executed when the project configuration changes.
-        // The event is the same as for 'onChange'.
     }
 
     def onShutdown = { event ->
-        // TODO Implement code that is executed when the application shuts down (optional)
     }
 }
